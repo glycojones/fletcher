28dac9683e5a4c61a8fe4153caadbc061a79ac7f
Jon Agirre <jon.agirre@york.ac.uk>
2025-02-10 10:31:34 +0000

    We added rotamer library functions and data from Iris - Thank you, Will!

--- src/fletcher/fletcher.py
+++ src/fletcher/fletcher.py
@@ -1,8 +1,72 @@
+import os
 import gemmi
 import argparse
+import gzip
 import json
+import pickle
+import itertools
+import numpy as np
 from pathlib import Path
 from math import exp
+
+DATA_DIR_PATH = os.path.join(os.path.dirname(__file__), 'data')
+LIBRARY_PATH = os.path.join(DATA_DIR_PATH, 'library.gz')
+library_data = None
+
+def product(x):
+    result = 1
+    for x_i in x:
+        result *= x_i
+    return result
+
+def unpack_bytes(in_bytes):
+    try:
+        masks = np.array([ 0b11000000, 0b00110000, 0b00001100, 0b00000011 ])
+        shifts = np.array([ 6, 4, 2, 0 ])
+        masked = np.array(in_bytes).reshape(-1, 1) & np.array(masks)
+        shifted = masked >> np.array(shifts)
+        unpacked = shifted.flatten().astype('int8')
+    except ImportError:
+        # Python-only mode
+        bits = list(itertools.product(range(4), repeat=4))
+        replaced = [ bits[b] for b in in_bytes ]
+        unpacked = [ bits for byte in replaced for bits in byte ]
+    return unpacked
+
+def load_rotamer_data():
+        with gzip.open(LIBRARY_PATH, 'rb') as infile:
+            dim_offsets, dim_bin_ranges, dim_bin_widths, dim_num_options, compressed_byte_arrays = pickle.load(infile)
+        classifications = { }
+        for code, compressed in compressed_byte_arrays.items():
+            compressed = bytearray(compressed)
+            classifications[code] = unpack_bytes(compressed)
+        library_data = (dim_offsets, dim_bin_ranges, dim_bin_widths, dim_num_options, classifications)
+
+def get_classification(code, chis):
+    dim_offsets, dim_bin_ranges, dim_bin_widths, dim_num_options, classifications = library_data
+    if  None in chis:
+        return
+    if code not in dim_offsets.keys():
+        return
+    closest_values = [ ]
+    chis = tuple([ x for x in chis if x is not None ][:len(dim_offsets[code])])
+    for dimension, chi in enumerate(chis):
+        dim_width = dim_bin_ranges[code][dimension][1] - dim_bin_ranges[code][dimension][0]
+        if chi <= dim_bin_ranges[code][dimension][0]:
+            chi += dim_width
+        if chi >= dim_bin_ranges[code][dimension][1]:
+            chi -= dim_width
+        multiple = round((chi - dim_offsets[code][dimension]) / dim_bin_widths[code][dimension])
+        closest_value = dim_offsets[code][dimension] + multiple * dim_bin_widths[code][dimension]
+        closest_values.append(closest_value)
+    closest_values = tuple(closest_values)
+    index = 0
+    for dimension, chi in enumerate(closest_values):
+        dim_offest = dim_offsets[code][dimension]
+        dim_bin_width = dim_bin_widths[code][dimension]
+        index += int((chi - dim_offest) / dim_bin_width * product(dim_num_options[code][dimension+1:]))
+    return classifications[code][index]
+
 
 def plddt_to_rmsd ( plddt = 0.0 ) :
   frac_lddt = plddt / 100.0
