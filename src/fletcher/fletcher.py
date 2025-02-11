89cf0b69cdfd50ac2e8f34e5d8cd906114da083a
Stephen-UoY <thestephenboxall@gmail.com>
2025-02-11 18:00:34 +0000

    Update fletcher.py

--- src/fletcher/fletcher.py
+++ src/fletcher/fletcher.py
@@ -180,6 +180,15 @@ def find_structural_motifs ( filename = "",
     print ("\nNo results found :-( \n")
   return result_dict
 
+def find_matching_residue(ref_model, residue):
+    """Finds the corresponding residue in the reference model based on sequence ID."""
+    for ref_chain in ref_model[0]:  # Assuming only one model in reference
+        for ref_residue in ref_chain:
+            if ref_residue.seqid == residue.seqid:  # Compare seqid
+                return ref_residue
+    return None  # Return None if no match is found
+
+
 if __name__ == '__main__':
   parser = argparse.ArgumentParser ( 
                     prog='Fletcher',
