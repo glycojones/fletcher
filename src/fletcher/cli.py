0270eee463eb4f5a453ca3c9ecbbdbd292c58bc7
Rebecca Taylor <rltaylor.453@gmail.com>
2025-08-31 19:06:05 +0100

    removed capability to add multiple query structures per flag to avoid confusion in syntax

--- src/fletcher/cli.py
+++ src/fletcher/cli.py
@@ -9,25 +9,55 @@ repo_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if repo_root not in sys.path:
     sys.path.insert(0, repo_root)
 
-from .fletcher import get_reference_neighbours, compare_queries_to_reference
+from .fletcher import get_reference_neighbours, compare_query_to_reference
 
 def main():
-    parser = argparse.ArgumentParser(description="Chemistry-aware lDDT-like scoring for PDB files")
-    parser.add_argument("--ref", required=True, help="Reference PDB path")
-    parser.add_argument("--queries", required=True, nargs="+", help="Query PDB files or glob pattern")
-    parser.add_argument("--target_chain", required=True, help="Target chain ID in reference structure")
-    parser.add_argument("--target_res", type=int, required=True, help="Target residue number in reference structure")
-    parser.add_argument("--results_dir", default="results", help="Directory to save results")
-    parser.add_argument("--distance_cutoff", type=float, default=15.0, help="Distance cutoff for neighbors")
-    parser.add_argument("--min_pairs_required", default=3, type=int,help="Minimum number of CA-atom pairs required to compute a score (default: 3)")
-    parser.add_argument("--lddt_thresholds", nargs='+', default=[0.5, 1.0, 2.0, 4.0], type=float,help="Distance thresholds for lDDT scoring (default: 0.5 1.0 2.0 4.0)")
+    parser = argparse.ArgumentParser(
+        prog = "Fletcher",
+        description = "Chemistry-aware lDDT-like scoring for PDB files",
+        epilog = "Example usage: fletcher --ref path/to/ref.pdb --queries path/to/query1.pdb path/to/query2.pdb --target_chain A --target_res 100")
+    
+    parser.add_argument('-r', '--ref', help = "Reference PDB path", required = True)
+    
+    parser.add_argument('-q', '--query', \
+                        help = "Query PDB file", \
+                        required = True)
+    
+    parser.add_argument('-c', '--target_chain', \
+                        help = "Target chain ID in reference structure", \
+                        required = True)
+    
+    parser.add_argument('-t', '--target_res', \
+                        help = "Target residue number in reference structure",
+                        type = int, 
+                        required = True)
+    
+    parser.add_argument('--results_dir', \
+                        default = "results", \
+                        help="Directory to save results")
+    
+    parser.add_argument('--distance_cutoff', \
+                        type=float, 
+                        default=15.0, 
+                        help="Distance cutoff for neighbors")
+    
+    parser.add_argument('--min_pairs_required', \
+                        type=int,
+                        default=3, 
+                        help="Minimum number of CA-atom pairs required to compute a score (default: 3)")
+    
+    parser.add_argument('--lddt_thresholds', \
+                        nargs='+', 
+                        default=[0.5, 1.0, 2.0, 4.0], 
+                        type=float,
+                        help="Distance thresholds for lDDT scoring (default: 0.5 1.0 2.0 4.0)")
 
     args = parser.parse_args()
 
-    # Expand glob patterns
-    query_paths = []
-    for pattern in args.queries:
-        query_paths.extend(glob.glob(pattern))
+    # # Expand glob patterns
+    # query_paths = []
+    # for pattern in args.queries:
+    #     query_paths.extend(glob.glob(pattern))
 
     target_residue_name, ref_neighbours = get_reference_neighbours(
         args.ref, 
@@ -36,10 +66,10 @@ def main():
         distance_cutoff=args.distance_cutoff
     )
 
-    compare_queries_to_reference(
+    compare_query_to_reference(
         target_residue_name, 
         ref_neighbours, 
-        args.queries,
+        args.query,
         distance_cutoff=args.distance_cutoff,
         min_pairs_required=args.min_pairs_required,
         lddt_thresholds=args.lddt_thresholds,
